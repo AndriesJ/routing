d842e4712e7bad197879bc44f7fad4a6d481d493
andriesj343-dev <andriesj343@gmail.com>
2026-03-06 11:40:50 +0200

    MAKE IT PWA

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'runroutes-v1';
+const RUNTIME_CACHE = 'runroutes-runtime-v1';
+
+// Assets to cache on install
+const PRECACHE_ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/js/app.js',
+  '/icons/icon-72.png',
+  '/icons/icon-96.png',
+  '/icons/icon-128.png',
+  '/icons/icon-144.png',
+  '/icons/icon-152.png',
+  '/icons/icon-192.png',
+  '/icons/icon-384.png',
+  '/icons/icon-512.png',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js',
+  '
